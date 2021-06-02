9ff019c5d4433a89b31ffb1d1a0a59d469ed2813
akirbaes <akirbaes@ulb.ac.be>
2021-06-02 10:21:26 +0200

    Fixed johnsons file

--- poly_dicts/johnson_nets.py
+++ poly_dicts/johnson_nets.py
@@ -1,5 +1,5 @@
 #dict[face]=[neighbour_face ...]
-#neighbour faces in clockwise orderjohnson_nets= dict()
+johnson_nets= dict()
 johnson_nets['j1'] = \
 {0: [1, 2, 3, 4], 1: [0, 4, 2], 2: [0, 1, 3], 3: [0, 2, 4], 4: [0, 3, 1]}
 
