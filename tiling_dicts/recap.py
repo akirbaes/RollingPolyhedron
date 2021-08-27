749cdce681738c2a697397be8748d08671b362ed
akirbaes <akirbaes@ulb.ac.be>
2021-08-27 09:35:55 +0200

    Update with 3-isogonals

--- tiling_dicts/recap.py
+++ tiling_dicts/recap.py
@@ -1,6 +1,7 @@
 from archimedean_tilings import archimedean_tilings
 from isogonal_tilings import biisogonal_tilings
 from platonic_tilings import platonic_tilings
-
-names = list(platonic_tilings.keys())+list(archimedean_tilings.keys())+list(biisogonal_tilings.keys())
+from triisogonal_vertex_homogeneous import triisogonal_vertex_homogeneous
+all_tilings = {**platonic_tilings, **archimedean_tilings, **biisogonal_tilings, **triisogonal_vertex_homogeneous}
+names = list(all_tilings)
 print(names)
