3bdebb42ddfd254e4d53be84fffc0f8a0c57bb95
akirbaes <akirbaes@ulb.ac.be>
2021-09-02 10:20:17 +0200

    Adapt for new uniform naming convention

--- tiling_dicts/platonic_tilings.py
+++ tiling_dicts/platonic_tilings.py
@@ -2,13 +2,13 @@
 #neighbour faces in clockwise order
 #match differenciator: -d with +d, else d with d
 platonic_tilings= dict()
-platonic_tilings['3^6'] = \
+platonic_tilings['1u01 (3^6)'] = \
 {0: [(1, 0), (1, 1), (1, 2)],
  1: [(0, 0), (0, -1), (0, -2)]}
 
-platonic_tilings['4^4'] = \
+platonic_tilings['1u02 (4^4)'] = \
 {0: [(0, 1), (0, 2), (0, -1), (0, -2)]}
 
-platonic_tilings['6^3'] = \
+platonic_tilings['1u03 (6^3)'] = \
 {0: [(0, 1), (0, 2), (0, 3), (0, -1), (0, -2), (0, -3)]}
 
