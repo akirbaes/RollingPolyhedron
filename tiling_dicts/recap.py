73f929c35445952e620ec9d1534e4f325725bc2c
akirbaes <akirbaes@ulb.ac.be>
2021-09-05 23:03:15 +0200

    Unused (see uniform_tiling.py)

--- tiling_dicts/recap.py
+++
@@ -1,7 +0,0 @@
-from archimedean_tilings import archimedean_tilings
-from isogonal_tilings import biisogonal_tilings
-from platonic_tilings import platonic_tilings
-from triisogonal_vertex_homogeneous import triisogonal_vertex_homogeneous
-all_tilings = {**platonic_tilings, **archimedean_tilings, **biisogonal_tilings, **triisogonal_vertex_homogeneous}
-names = list(all_tilings)
-print(names)
