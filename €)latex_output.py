024e64137fd7cba167a5d956b3f7143b12aa71cc
akirbaes <akirbaes@ulb.ac.be>
2022-03-24 23:19:10 +0100

    Counts table output trial

--- €)latex_output.py
+++ €)latex_output.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 import os
 import pickle
 
@@ -12,7 +13,7 @@ from tiling_dicts.archimedean_tilings import archimedean_tilings
 # from tiling_dicts.isogonal_tilings import biisogonal_tilings
 # from tiling_dicts.triisogonal_vertex_homogeneous import triisogonal_vertex_homogeneous
 # all_tilings = {**platonic_tilings, **archimedean_tilings, **biisogonal_tilings, **triisogonal_vertex_homogeneous}
-from tiling_dicts.uniform_tilings import uniform_tilings
+from tiling_dicts.combine_uniform_tilings import uniform_tilings
 
 all_tilings  = {**uniform_tilings}
 print(len(all_tilings))
@@ -23,76 +24,6 @@ all_nets = {**plato_archi_nets, **johnson_nets, **prism_nets}
 all_nets_dict = {**plato_archi_nets, **johnson_nets, **prism_nets}
 
 from DrawingFunctions import turn_into_image
-tileurl = """https://i.imgur.com/0NvVetj.png
-https://i.imgur.com/eBjngcx.png
-https://i.imgur.com/5a9KBJT.png
-https://i.imgur.com/O35WIdn.png
-https://i.imgur.com/cFMHQEg.png
-https://i.imgur.com/QunlcxR.png
-https://i.imgur.com/BR3iVNA.png
-https://i.imgur.com/kRvEAYw.png
-https://i.imgur.com/HiK00HY.png
-https://i.imgur.com/wkMNhb4.png
-https://i.imgur.com/DY3tRhP.png
-https://i.imgur.com/kSvh4XE.png
-https://i.imgur.com/pfMgmVE.png
-https://i.imgur.com/FQxRbGO.png
-https://i.imgur.com/6py4GES.png
-https://i.imgur.com/bES75Zp.png
-https://i.imgur.com/yrkd2Td.png
-https://i.imgur.com/yYlmPFb.png
-https://i.imgur.com/YYohMiu.png
-https://i.imgur.com/spTeWTF.png
-https://i.imgur.com/ah4STKx.png
-https://i.imgur.com/dl7gFm3.png
-https://i.imgur.com/E38mCuu.png
-https://i.imgur.com/78iFOLz.png
-https://i.imgur.com/wKxSBeF.png
-https://i.imgur.com/0YIadhU.png
-https://i.imgur.com/WdThPkO.png
-https://i.imgur.com/W6DaIXo.png
-https://i.imgur.com/Qjqeo0Z.png
-https://i.imgur.com/onomQdI.png
-https://i.imgur.com/8xbc2uw.png
-https://i.imgur.com/Xf51OOa.png
-https://i.imgur.com/EIshIi3.png
-https://i.imgur.com/Xw0TiuP.png
-https://i.imgur.com/04RFAi4.png
-https://i.imgur.com/7BsKbSN.png
-https://i.imgur.com/G2zlHYt.png
-https://i.imgur.com/ICtYQ54.png
-https://i.imgur.com/Ye5e7T3.png
-https://i.imgur.com/y7K63i8.png
-https://i.imgur.com/oyP65JS.png
-https://i.imgur.com/E06cGnX.png
-https://i.imgur.com/J7Trahc.png
-https://i.imgur.com/aZGEdNv.png
-https://i.imgur.com/fg8gUOV.png
-https://i.imgur.com/EO2MDBD.png
-https://i.imgur.com/4IQOe3I.png
-https://i.imgur.com/6uYGA78.png
-https://i.imgur.com/E1CfT4o.png
-https://i.imgur.com/o2VM5qE.png
-https://i.imgur.com/ragnfbp.png
-https://i.imgur.com/9ZPDs3o.png
-https://i.imgur.com/SMNF4s0.png
-https://i.imgur.com/0qFXuWn.png
-https://i.imgur.com/IQgKp70.png
-https://i.imgur.com/CNpAjhe.png
-https://i.imgur.com/44o1BpV.png
-https://i.imgur.com/0fcht32.png
-https://i.imgur.com/xHTgCGe.png
-https://i.imgur.com/oXXut5a.png
-https://i.imgur.com/rYN8pBq.png
-https://i.imgur.com/cWrJj6w.png
-https://i.imgur.com/5GZ8gzX.png
-https://i.imgur.com/SzWlfz5.png
-https://i.imgur.com/Wu6THrL.png
-https://i.imgur.com/xIMJ5V9.png
-https://i.imgur.com/SNJ9Ix5.png
-https://i.imgur.com/Ubo4SVS.png
-https://i.imgur.com/ead6MaS.png
-https://i.imgur.com/QWeVUSi.png""".split()
 
 johnsonurls = [
 ("https://en.wikipedia.org/wiki/Johnson_solid#Pyramids",(1,2)),
@@ -119,7 +50,7 @@ johnsonurls = [
 
 
 polyurls = {
-    polyname:"https://en.wikipedia.org/wiki/%s"%(polyname.replace("_c","")) for polyname in list(plato_archi_nets)+list(prism_nets)
+    polyname:"https://en.wikipedia.org/wiki/%s"%(polyname[:-2] if polyname.endswith("_c") else polyname) for polyname in list(plato_archi_nets)+list(prism_nets)
 }
 
 # "cube": "https://en.wikipedia.org/wiki/Cube",
@@ -147,13 +78,36 @@ def find_roller_withname(tilename,polyname):
                 return name
     raise FileNotFoundError(polyname+" @ "+tilename)
 
+def find_tiling_url(tilename):
+    number = str(list(all_tilings).index(tilename)+1)+" "
+    path = "saved_results\Imgur Album kuniform tessellations tile symmetries and periodic translation area"
+    for dirpath, dirnames, filenames in os.walk(path):
+        for name in filenames:
+            if name.startswith(number):
+
+                url = "https://i.imgur.com/%s"%(name.split(" ")[-1])
+                return url
+
+def find_roller_url(planerollerid):
+    path= "saved_results\Imgur Album Plane rollers starting position hint"
+    number = str(planerollerid+1)+" "
+    for dirpath, dirnames, filenames in os.walk(path):
+        for name in filenames:
+            if name.startswith(number):
+
+                url = "https://i.imgur.com/%s"%(name.split(" ")[-1])
+                return url
+
+def roller_id(til,poly,rollersdata):
+    return list(rollersdata).index((til,poly))
+
 def find_roller_face_withname(tilename,polyname):
     # path = "_proofimages_backup/rollers_withfaces"
     path="rolled_nets"
     for dirpath, dirnames, filenames in os.walk(path):
 
         for name in filenames:
-            if name.startswith(tilename+" @ "+polyname):
+            if (tilename+" @ "+polyname) in name:
 
             # if (name.endswith(polyname+" rolls the "+tilename+" tiling.png")):
                 return name
@@ -182,7 +136,7 @@ def output_condensedtable(all_nets,all_tilings,rollersdata):
             for (tilename,polyname),value in rollersdata.items():
                 if tile == tilename: #n², a better way would be sorting them by the first list first
                     try:
-                        url = tileurl[all_tilings.index(tilename)]
+                        url = find_tiling_url(tilename)
                         tileref = "\href{%s}{$%s$}"%(url,tilename)
                     except:
                         print(tilename,"has no url")
@@ -303,12 +257,13 @@ def output_rollerstable(all_nets,all_tilings,rollingresults, mode="complement"):
                         or (tilename in archimedean_tilings and "archimedean_tiling_rollers") \
                         or (tilename.startswith("2") and "2isogonal_tiling_rollers") \
                         or (tilename.startswith("3") and "3isogonal_tiling_rollers") \
+                        or (tilename.startswith("4") and "4isogonal_tiling_rollers") \
                         or "unknown tiling group"
             if mode=="main":
                 linedata.append("\hypertarget{%s}{"%hypertarget+"\makecell{%s \\\\ $%s$ \\\\ \hyperlink{%s}{back}}}"%(polyname,tilename,hyperlink))
             elif mode=="complement":
                 polyref = "\href{%s}{%s}"%(polyurls[polyname],polyname.replace("_"," "))
-                tileref = "\href{%s}{$%s$}" % (tileurl[all_tilings.index(tilename)], tilename)
+                tileref = "\href{%s}{$%s$}" % (find_tiling_url(tilename), tilename)
                 linedata.append("\hypertarget{%s}{"%hypertarget+"\makecell{%s \\\\ %s \\\\ }}"%(polyref,tileref))
 
             filename = find_roller_withname(tilename,polyname)
@@ -323,9 +278,10 @@ def output_rollerstable(all_nets,all_tilings,rollingresults, mode="complement"):
                     "\\raisebox{-.5\height}{\includegraphics[width=100pt]{rolls/proofrolls/rollerstability/%s}}" %
                     (polyname+"@"+tilename+" stability.png"))
             # print(tilename,polyname)
+            fileurl = find_roller_url(roller_id(tilename,polyname,rollingresults))
             filename = find_roller_face_withname(tilename,polyname)
             linedata.append(
-                "\\raisebox{-.5\height}{\includegraphics[width=100pt]{rolls/proofrolls/rollerswithfaces/%s}}" %filename)
+                "\\raisebox{-.5\height}{\includegraphics[width=100pt]{rolls/proofrolls/rollerswithfaces/%s}}" %(filename))
             if(results["stability"]):
                 stablelines.append(linedata)
             else:
@@ -387,6 +343,126 @@ def output_table(all_nets,all_tilings,rollersdata):
 \end{center}""")
         f.write("\n")
 
+def output_htmlmatrix(all_nets, all_tilings, rollersdata):
+    print("Output htmlmatrix of",len(all_nets),"*",len(all_tilings))
+    characters = "  ◙◘#↔×• "#"█■▒▩▩↔x*?" #━ "█■▒▩▩↔x*❌"◙○◘•
+    """.SPR {background-color: red; color: red;}
+.PR {background-color: brown; color: brown;}
+.SQPR {background-color: DarkBlue; color: #0080FF;}
+.QPR {background-color: DarkBlue; color: blue;}
+.HPR {background-color: DarkBlue; color: BlueViolet;}
+.br {background-color: Green; color: GhostWhite;}
+.ar { color: Green;}
+.x {color: Green;}"""
+    output = """<!DOCTYPE html>
+<html>
+<head>
+<style>
+tr.tilingnames td {writing-mode: vertical-rl;vertical-align: baseline;transform: rotate(-180deg);}
+body {
+  font-family: 'Courier New', monospace;
+  font-size: 8px;
+  horizontal-align: right;
+}
+img { display: block; }
+tr:hover { background: yellow; }
+tr.tilingnames:hover {background: white;  }
+td, tr, img  { padding: 0px; margin: 0px; border: none; }
+table { border-collapse: collapse; }
+</style>
+</head>
+"""
+    #border-collapse:collapse;
+
+    values = ["SPR","PR","SQPR","QPR","HPR","br","ar","x","_"]
+    descriptions = ["Stable Plane Roller", "Other Plane Roller", "Stable Quasi-Plane Roller", "Other Quasi-Plane Roller", "Hollow-Plane Roller", "Band Roller", "Area Roller", "Area Roller (cannot escape starting tile)", "Incompatible"]
+
+    output += '<table >\n'
+    for i in range(len(values)):
+        output+='  <tr><td><img src=%s.png></td><td>%s<td></tr>\n'%(values[i],descriptions[i])
+    output += '</table >\n'
+
+
+    output += '<table >\n  <tr class="tilingnames">\n    <td/>\n'
+    for tiling in all_tilings:
+        shortile = tiling.split(" ")[0]
+        output+='    <td><a href="t/%s.png">%s</a></td>\n'%(shortile,shortile) #?page=
+    output+= '</tr>'
+
+    for net in all_nets:
+        shortnet = net
+        # shortnet = shortnet.replace("triangular_","▲-")
+        # shortnet = shortnet.replace("square_","■-")
+        # shortnet = shortnet.replace("pentagonal_","⬟-")
+        # shortnet = shortnet.replace("hexagonal_","⬣-")
+        # shortnet = shortnet.replace("octagonal_","⯃-")
+        # shortnet = shortnet.replace("dodecagonal_","12-")
+        # shortnet = shortnet.replace("decagonal_","10-")
+        shortnet = shortnet.replace("triangular_","3-")
+        shortnet = shortnet.replace("square_","4-")
+        shortnet = shortnet.replace("pentagonal_","5-")
+        shortnet = shortnet.replace("hexagonal_","6-")
+        shortnet = shortnet.replace("octagonal_","8-")
+        shortnet = shortnet.replace("dodecagonal_","12-")
+        shortnet = shortnet.replace("decagonal_","10-")
+        output+= '  <tr>\n    <td><a href="p/%s.png">%s</a></td>\n'%(net,shortnet)
+
+        for tiling in all_tilings:
+            data = rollersdata[(tiling,net)]
+            # outchar = characters[values.index(data)]
+
+            shortile = tiling.split(" ")[0]
+
+            image = "<img src=%s.png>"%(data.replace(" ","_"))
+            outchar = '<a href="r.php?p=%s&amp;t=%s&amp;r=%s">%s</a></td>\n' % (net,shortile, data.replace(" ","_"), image)  # ?page=
+            # output+='    <td class="%s">%s</td>'%(data,outchar)
+            output+='    <td>%s</td>'%(outchar)
+
+        output+= '</tr>'
+    output+="\n</table>\n</html>"
+
+    with open(".html_output/index.html","w", encoding="utf-8") as f:
+        f.write(output)
+
+
+
+def output_rollingpairs(all_nets,all_tilings,rollersdata):
+    all_plane_roller_pairs = []
+    for (tile,net),value in rollersdata.items():
+        if(value in ("SPR","SQPR")):
+            all_plane_roller_pairs.append((net,tile))
+    with open(".latex_output/stable_pairs.py","w") as f:
+        f.write(str(all_plane_roller_pairs))
+
+
+def output_counters_table(all_nets,all_tilings,rollersdata):
+    print("Counts table output")
+    #{'ar', 'HPR', 'br', 'QPR', 'PR', 'SQPR', 'x', ' ', 'SPR'}
+    f = open(".latex_output/rollers_counters_table.tex","w")
+    # f.write("Number of rollers of the given type found on all considered tessellations:\n\n")
+    f.write("""\\begin{center}
+\\begin{longtable}{ | c | c | c | c | c |}
+\hline \n""")
+#\caption{Number of rollers of the given type found on all considered tessellations.\label{counters}}\n""")
+
+    titles = "Polyhedron","Plane roller","Hollow plane","Band","Area$>$1"
+
+    f.write(" & ".join(titles) + " \\\\ \n\hline\hline\n")
+    for net in all_nets:
+        counts = [0,0,0,0,0]
+        index = {"PR":0,"SPR":0,"SQPR":1,"QPR":1,"HPR":1,"br":2,"ar":3,"x":4," ":4}
+        for tile in all_tilings:
+            counts[index[rollersdata[(tile,net)]]]+=1
+        counts.pop(-1)
+        netter = net.replace("_"," ")
+        neturl = "\href{%s}{%s}"%(polyurls[net],netter+"hiral" if netter.endswith(" c") else netter)
+        #neturl = net.replace("_"," ")
+        if(counts[0]):
+            f.write(" & ".join([neturl]+[str(c) for c in counts])+" \\\\ \hline ")
+
+    f.write("""\n\end{longtable}
+\end{center}""")
+    f.close()
 
 
 def output_whitematrix(all_nets,all_tilings,rollersdata):
@@ -483,6 +559,36 @@ def write_polyherons_list():
 #     GenPngScreenspaceRoller.draw_answer(filename, tilingname, polyname, visits, grid, polyhedron, p1, p2, startface, startorientation, w, h,
 #                 unusedfaces)
 
+def write_rollable_polyhedron_list(all_nets,all_tilings,rollersdata):
+
+    all_poly = set(all_nets)
+    all_rollables = set()
+    rollable_pairs = dict()
+    unrolled  =list()
+    for i,net in enumerate(all_nets):
+        all_poly.add(net)
+        for j,tile in enumerate(all_tilings):
+            if rollersdata[(tile,net)] in ("SPR","PR"):
+                all_rollables.add((net))
+                rollable_pairs[net]=rollable_pairs.get(net,[])+[tile]
+        if net not in rollable_pairs:
+            unrolled.append(net)
+    # unrolled = all_poly-all_rollables
+    with open(".latex_output/polyhedron_rollerslist.txt","w") as f:
+        f.write("# Polyhedron found in plane roller: %i\n"%len(all_rollables))
+        for poly,tilings in rollable_pairs.items():
+            # for i,tile in enumerate(tilings):
+                # if(" " in tile):
+                #     tilings[i]=tile[:tile.index(" ")]
+            f.write("%s: %s\n"%(poly,", ".join(tilings)))
+        f.write("# Polyhedron without plane roller found: %i\n"%len(unrolled))
+        for poly in unrolled:
+            f.write(poly+"\n")
+        f.write("Total poly: %i\n"%len(all_poly))
+
+import pprint
+import json
+
 if __name__ == "__main__":
     write_tilings_list()
     write_polyherons_list()
@@ -494,10 +600,26 @@ if __name__ == "__main__":
     with open("rolling_results.pickle", "rb") as handle:
         rollingresults = pickle.load(handle)
 
+    # new_object = dict()
+    # print(rollingresults)
+    # json_object=pprint.pformat(rollingresults,indent=2)
+    # print(json_object)
+    # json_object = json.dumps(rollingresults, indent=4)
+    # with open("rolling_results.json", "w") as f:
+    #     f.write(json_object)
+    # exit()
+
+    with open(".html_output/polyurls.txt", "w", encoding="utf-8") as f:
+        f.write("$polyurls = [\n")
+        for poly, url in polyurls.items():
+            f.write('    "%s" => "%s",\n' % (poly, url))
+        f.write("];")
+
+    output_counters_table(all_nets,all_tilings,rollersdata)
     output_whitematrix(all_nets,all_tilings,rollersdata)
-
-
-
+    output_htmlmatrix(all_nets,all_tilings,rollersdata)
+    output_rollingpairs(all_nets,all_tilings,rollersdata)
+    write_rollable_polyhedron_list(all_nets,all_tilings,rollersdata)
 
 
     # print(len(all_tilings))
@@ -512,4 +634,15 @@ if __name__ == "__main__":
     output_rollerstable(all_nets,all_tilings,rollingresults)
     output_quasirollerstable(all_nets, all_tilings, rollingresults)
 
+    import json
+    # print(rollingresults)
+    # print(json.dumps(rollersdata,indent=4))
+    with open(".latex_output/rollersdata.py","w") as f:
+        f.write("rollersdata = \\\n")
+        f.write(pprint.pformat(rollersdata,indent=1,sort_dicts=False))
+        # f.write(json.dumps(rollersdata,indent=4))
+    with open(".latex_output/rollingresults.py","w") as f:
+        f.write("rollingresults = \\\n")
+        f.write(pprint.pformat(rollingresults,indent=1,sort_dicts=False))
+        # f.write(json.dumps(rollingresults,indent=4))
 
